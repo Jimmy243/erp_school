72d1be95a8495c892cf7550515364ab75dd08347
unknown <33499786+sadicky@users.noreply.github.com>
2024-12-23 18:46:14 +0200

    Init

--- resources/views/layouts/public/pied.blade.php
+++ resources/views/layouts/public/pied.blade.php
@@ -13,20 +13,23 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-   <script src="{{asset('public/assets/node_modules/jquery/jquery-3.2.1.min.js')}}"></script>
+  <script src="{{asset('public/assets/node_modules/jquery/jquery-3.2.1.min.js')}}"></script>
+  <script src="{{asset('public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('public/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('public/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('public/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('public/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('public/assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
+
   <!-- Template Main JS File -->
   <script src="{{asset('public/assets/js/main.js')}}"></script>
 
    <!--tags input-->
-    <script src="{{asset('public/input/jquery-tags-input.js')}}" ></script>
+    <script src="{{asset('public/dist/js/pages/mask.init.js')}}"></script>
+     <script src="{{asset('public/assets/node_modules/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
+    <script src="{{asset('public/assets/node_modules/sparkline/jquery.sparkline.min.js')}}"></script>
        <!-- Sweet-Alert  -->
    <!-- <script src='public/dist/js/api.js'></script> -->
-<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-    async defer>
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer>
 </script>
-
-
-
 </body>
-
-</html>
